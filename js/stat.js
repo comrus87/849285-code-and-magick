63caf3d22ac6aacd44947231cf17a48bb436b70c
Andrew <nk-andrew1987@mail.ru>
2018-11-26 19:41:42 +0300

    Выполнено второе задание

--- js/stat.js
+++ js/stat.js
@@ -6,7 +6,6 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
 var BAR_GAP = 50;
-// var TEXT_WIDTH = 50;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var FONT_HEIGHT = 16;
@@ -24,7 +23,6 @@ var getMaxElement = function (arr) {
       maxElement = arr[i];
     }
   }
-
   return maxElement;
 };
 
@@ -33,11 +31,24 @@ window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 4, CLOUD_Y + GAP * 3);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP * 4, CLOUD_Y + GAP * 4 + FONT_HEIGHT / 2);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], CLOUD_X + GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - GAP);
-    ctx.fillRect(CLOUD_X + GAP + (BAR_WIDTH + BAR_GAP) * i, CLOUD_Y + CLOUD_HEIGHT - (GAP * 2 + FONT_HEIGHT), (BAR_WIDTH, BAR_HEIGHT * times[i]) / maxTime);
+    var totalBarHeight = BAR_HEIGHT * times[i] / maxTime;
+    var sizeElementX = CLOUD_X + GAP * 4 + (BAR_GAP + BAR_WIDTH) * i;
+    var sizeElementY = CLOUD_Y + CLOUD_HEIGHT - (GAP * 2 + FONT_HEIGHT + totalBarHeight);
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], sizeElementX, CLOUD_Y + CLOUD_HEIGHT - GAP);
+    ctx.fillText(Math.round(times[i]), sizeElementX, sizeElementY - GAP);
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random().toString() + ')';
+    }
+    ctx.fillRect(sizeElementX, sizeElementY, BAR_WIDTH, totalBarHeight);
   }
 };
